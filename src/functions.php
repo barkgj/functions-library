a41774cf1b4701e9f7d42cbba3861a4afcccda81
Gert-Jan Bark <1948488+barkgj@users.noreply.github.com>
2023-01-07 00:40:22 +0100

    etc

--- src/functions.php
+++ src/functions.php
@@ -122,8 +122,24 @@ final class functions
 	// newguid createguid
 	public static function create_guid($namespace = '') 
 	{
-		// kudos to https://stackoverflow.com/questions/21671179/how-to-generate-a-new-guid
-		return trim(com_create_guid(), '{}');
+		if (function_exists('com_create_guid'))
+		{
+			$result = trim(com_create_guid(), '{}');
+		}
+		else 
+		{
+			mt_srand((double)microtime()*10000);//optional for php 4.2.0 and up.
+			$charid = strtoupper(md5(uniqid(rand(), true)));
+			$hyphen = chr(45);// "-"
+			$uuid = ""
+				.substr($charid, 0, 8).$hyphen
+				.substr($charid, 8, 4).$hyphen
+				.substr($charid,12, 4).$hyphen
+				.substr($charid,16, 4).$hyphen
+				.substr($charid,20,12);
+			$result = $uuid;
+		}
+		return $result;
 	}
 
 	// pretty_print
