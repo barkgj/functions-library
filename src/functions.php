02c7a8e6c42b2ae352e975403de3eb7b29e2a4b3
Gert-Jan Bark <1948488+barkgj@users.noreply.github.com>
2023-01-06 22:59:15 +0100

    etc

--- src/functions.php
+++ src/functions.php
@@ -122,27 +122,8 @@ final class functions
 	// newguid createguid
 	public static function create_guid($namespace = '') 
 	{
-		// credits: http://php.net/manual/en/public static function.uniqid.php
-	static $guid = '';
-	$uid = uniqid("", true);
-	$data = $namespace;
-	$data .= $_SERVER['REQUEST_TIME'];
-	$data .= $_SERVER['HTTP_USER_AGENT'];
-	$data .= $_SERVER['LOCAL_ADDR'];
-	$data .= $_SERVER['LOCAL_PORT'];
-	$data .= $_SERVER['REMOTE_ADDR'];
-	$data .= $_SERVER['REMOTE_PORT'];
-	$hash = strtoupper(hash('ripemd128', $uid . $guid . md5($data)));
-	$guid = substr($hash,  0,  8) . 
-			'-' .
-			substr($hash,  8,  4) .
-			'-' .
-			substr($hash, 12,  4) .
-			'-' .
-			substr($hash, 16,  4) .
-			'-' .
-			substr($hash, 20, 12);
-	return $guid;
+		// kudos to https://stackoverflow.com/questions/21671179/how-to-generate-a-new-guid
+		return trim(com_create_guid(), '{}');
 	}
 
 	// pretty_print
